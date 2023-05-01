27c890b1f847517405c329c7f0912ae0b789f9de
Nicholas Le <nicholasl.le224@gmail.com>
2023-04-30 22:01:27 -0700

    assets

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Space space = new Space(1000, 1000, false, 1f, 0.0f);
         space.addParticle(500, 500, 0, 0, 50, 1000, Color.ORANGE);
-        space.addParticle(250, 500, 0, 10, 10, 10, Color.BLUE);
-        space.addParticle(750, 500, 0, -10, 10, 10, Color.BLUE);
+        space.addParticle(200, 500, 0, 10, 10, 10, Color.BLUE);
+        space.addParticle(180, 500, 0, 10, 5, 5, Color.GRAY);
+        space.addParticle(800, 500, 0, -10, 10, 10, Color.BLUE);
+        space.addParticle(820, 500, 0, -10, 5, 5, Color.GRAY);
         space.start();
     }
 }
+
